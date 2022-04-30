9eb759731866af6b84c3c1958b88422364bd571b
MDABDULMALIK <abdulmalikjmi6@gmail.com>
2022-04-30 15:59:28 +0530

    Add project files.

---
+++ WebApplication40/Controllers/SwiggyController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication40.Controllers
+{
+    public class SwiggyController : Controller
+    {
+        // GET: Swiggy
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Login()
+        {
+            return View();
+        }
+        public ActionResult menu()
+        {
+            return View();
+        }
+
+        public ActionResult  Aboutus()
+        {
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            return View();
+        }
+        public ActionResult Settings()
+        {
+            return View();
+        }
+        public ActionResult carrier()
+        {
+            return View();
+        }
+
+
+    }
+}
